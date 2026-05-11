10078df8bb8dd5d998295f727c975b32ef11ae6a
Weinan Liu <weinliu@redhat.com>
2026-05-10 21:10:35 -0400

    Add K6 load test and GitHub Actions integration

---
+++ k6-test.js
@@ -0,0 +1,18 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export let options = {
+  vus: 10,
+  duration: '30s',
+};
+
+export default function () {
+  let res = http.get('https://jsonplaceholder.typicode.com/posts/1');
+
+  check(res, {
+    'status is 200': (r) => r.status === 200,
+    'response time < 500ms': (r) => r.timings.duration < 500,
+  });
+
+  sleep(1);
+}
